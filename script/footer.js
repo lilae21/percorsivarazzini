3b0128d1085387acbbf6d954a01a0f8faff0a041
Elias Vecchio <elivec21@gmail.com>
2026-01-10 10:44:00 +0100

    Add files via upload

--- script/footer.js
+++ script/footer.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-   /*  fetch("https://lilae21.github.io/conlangs/parts/footer.htm") */
-   fetch("footer.htm")
+   fetch("https://lilae21.github.io/percorsivarazzini/script/footer.htm")
         .then(response => response.text())
         .then(html => {
             document.body.insertAdjacentHTML("beforeend", html);
